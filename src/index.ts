e62aa3cd8deb4af0c3da8d772b930dadde5f8581
Sami-zzz <2787621724@qq.com>
2024-03-07 20:56:40 +0800

    数据库环境配置

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-// 一定要将import './init';放到最开头,因为它里面初始化了路径别名
 import './init/alias';
+import { connectMysql } from './config/mysql';
 
 async function main() {
-  // await connectMysql();
+  await connectMysql();
   await (await import('./setup')).setupKoa();
 }
 
